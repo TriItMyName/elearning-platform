693e35d8c750dd8f952c98455b501a5d4c8d34dd
Phat Dat Nguyen <nguyenpdat782004@gmail.com>
2026-06-04 01:13:49 +0700

    first commit

---
+++ src/test/java/com/weblearning/WeblearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weblearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
