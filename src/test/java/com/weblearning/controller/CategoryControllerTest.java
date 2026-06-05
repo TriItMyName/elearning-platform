bce088248008a40819f6c73b137831b3dcea3a1b
Trần Trọng Trí <161337259+TriItMyName@users.noreply.github.com>
2026-06-05 10:20:57 +0700

    Cau hinh ket noi sql, cap nhat test junit

--- src/test/java/com/weblearning/controller/CategoryControllerTest.java
+++ src/test/java/com/weblearning/controller/CategoryControllerTest.java
@@ -9,8 +9,8 @@ import com.weblearning.service.CategoryService;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,10 +30,9 @@ class CategoryControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @MockBean
+    @MockitoBean
     private CategoryService categoryService;
 
     @Test
